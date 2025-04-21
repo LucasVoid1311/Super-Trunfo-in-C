b837d2e359dd69249b8aa1bff72864e223ab7e88
LucasVoid1311 <lucas.gabriel.zollner@gmail.com>
2025-04-21 11:39:23 -0300

    Update app.c

--- app.c
+++ app.c
@@ -77,6 +77,12 @@ int main() {
     float densi_pop2 = (float) (populacao2 / area2);
     float pib_p_capita2 = (float) (pib2 * 1000000000) / populacao2;
 
+    unsigned long int densi_PIB1 = densi_pop1 + pib_p_capita1;
+    unsigned long int densi_PIB2 = densi_pop2 + pib_p_capita2;
+
+    float super_power1 = (float) (populacao1 + area1 + pib1 + turistico1 + pib_p_capita1) / densi_pop1;
+    float super_power2 = (float) (populacao2 + area2 + pib2 + turistico2 + pib_p_capita2) / densi_pop2;
+
     printf("Carta: 1\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
@@ -87,6 +93,7 @@ int main() {
     printf("Número de Pontos Turísticos: %d\n", turistico1);
     printf("Densidade Populacional: %.2f hab/km²\n", densi_pop1);
     printf("PIB per Capita: %.2f reais\n", pib_p_capita1);
+    printf("Super poder: %.2f\n\n", super_power1);
 
     printf("\nCarta: 2\n");
     printf("Estado: %c\n", estado2);
@@ -98,6 +105,16 @@ int main() {
     printf("Número de Pontos Turísticos: %d\n", turistico2);
     printf("Densidade Populacional: %.2f hab/km²\n", densi_pop2);
     printf("PIB per Capita: %.2f reais\n", pib_p_capita2);
+    printf("Super poder: %.2f\n", super_power2);
+
+    printf("\nComparação de cartas:\n");
+    printf("População: %d\n", populacao1 > populacao2);
+    printf("Área: %d\n", area1 > area2);
+    printf("PIB: %d\n", pib1 > pib2);
+    printf("Pontos Turísticos: %d\n", turistico1 > turistico2);
+    printf("Densidade Populacional: %d\n", densi_pop1 < densi_pop2);
+    printf("PIB per Capita: %d\n", pib_p_capita1 > pib_p_capita2);
+    printf("Super Poder: %d\n", super_power1 > super_power2);
 
     return 0;
 }
